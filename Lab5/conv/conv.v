24e361c3e4788f41c946e26a41d65a9e3c10966e
kimpb <kasira09@kau.kr>
2022-06-08 04:11:51 +0900

    asd

--- Lab5/conv/conv.v
+++ Lab5/conv/conv.v
@@ -13,106 +13,116 @@ module CONV_MODULE(
 
 );
 
-reg [7:0] data00;
-reg [7:0] data01;
-reg [7:0] data02;
-reg [7:0] data10;
-reg [7:0] data11;
-reg [7:0] data12;
-reg [7:0] data20;
-reg [7:0] data21;
-reg [7:0] data22;
+reg signed [8:0] data00;
+reg signed [8:0] data01;
+reg signed [8:0] data02;
+reg signed [8:0] data10;
+reg signed [8:0] data11;
+reg signed [8:0] data12;
+reg signed [8:0] data20;
+reg signed [8:0] data21;
+reg signed [8:0] data22;
 
-reg [7:0] filter00;
-reg [7:0] filter01;
-reg [7:0] filter02;
-reg [7:0] filter10;
-reg [7:0] filter11;
-reg [7:0] filter12;
-reg [7:0] filter20;
-reg [7:0] filter21;
-reg [7:0] filter22;
+reg signed [8:0] filter00;
+reg signed [8:0] filter01;
+reg signed [8:0] filter02;
+reg signed [8:0] filter10;
+reg signed [8:0] filter11;
+reg signed [8:0] filter12;
+reg signed [8:0] filter20;
+reg signed [8:0] filter21;
+reg signed [8:0] filter22;
 
+wire signed [17:0] sum = data00 * filter00 + data01 * filter01 + data02 * filter02 +
+                         data10 * filter10 + data11 * filter11 + data12 * filter12 +
+                         data20 * filter20 + data21 * filter21 + data22 * filter22;
 
-wire [16:0] conv_result = data00 * filter00 + data01 * filter01 + data02 * filter02 +
-                          data10 * filter10 + data11 * filter11 + data12 * filter12 +
-                          data20 * filter20 + data21 * filter21 + data22 * filter22;
+wire [7:0] conv_result = (sum<0) ? 0 : (sum>8'hff)? 8'hff : sum[7:0];
 
 
 
 
 always @ (posedge clk)
     if(cs & write & (addr == 5'd0)) begin
-        if(byteenable ) data00[7:0] <= wdata[7:0];
+        if(byteenable) data00[8:0] <= {1'b0, wdata[7:0]};
     end
 
 always @ (posedge clk)
     if(cs & write & (addr == 5'd1)) begin
-        if(byteenable) data01[7:0] <= wdata[7:0];
+        if(byteenable) data01[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd2)) begin
-        if(byteenable) data02[7:0] <= wdata[7:0];
+        if(byteenable) data02[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd3)) begin
-        if(byteenable) data10[7:0] <= wdata[7:0];
+        if(byteenable) data10[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd4)) begin
-        if(byteenable) data11[7:0] <= wdata[7:0];
+        if(byteenable) data11[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd5)) begin
-        if(byteenable) data12[7:0] <= wdata[7:0];
+        if(byteenable) data12[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd6)) begin
-        if(byteenable) data20[7:0] <= wdata[7:0];
+        if(byteenable) data20[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd7)) begin
-        if(byteenable) data21[7:0] <= wdata[7:0];
+        if(byteenable) data21[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd8)) begin
-        if(byteenable) data22[7:0] <= wdata[7:0];
+        if(byteenable) data22[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd9)) begin
-        if(byteenable) filter00[7:0] <= wdata[7:0];
+        if(byteenable & wdata[7]) filter00[8:0] <= {1'b1, wdata[7:0]};
+        else if(byteenable & (~wdata[7])) filter00[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd10)) begin
-        if(byteenable) filter01[7:0] <= wdata[7:0];
+        if(byteenable & wdata[7]) filter01[8:0] <= {1'b1, wdata[7:0]};
+        else if(byteenable & (~wdata[7])) filter01[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd11)) begin
-        if(byteenable) filter02[7:0] <= wdata[7:0];
+        if(byteenable & wdata[7]) filter02[8:0] <= {1'b1, wdata[7:0]};
+        else if(byteenable & (~wdata[7])) filter02[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd12)) begin
-        if(byteenable) filter10[7:0] <= wdata[7:0];
+        if(byteenable & wdata[7]) filter10[8:0] <= {1'b1, wdata[7:0]};
+        else if(byteenable & (~wdata[7])) filter10[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd13)) begin
-        if(byteenable) filter11[7:0] <= wdata[7:0];
+        if(byteenable & wdata[7]) filter11[8:0] <= {1'b1, wdata[7:0]};
+        else if(byteenable & (~wdata[7])) filter11[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd14)) begin
-        if(byteenable) filter12[7:0] <= wdata[7:0];
+        if(byteenable & wdata[7]) filter12[8:0] <= {1'b1, wdata[7:0]};
+        else if(byteenable & (~wdata[7])) filter12[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd15)) begin
-        if(byteenable) filter20[7:0] <= wdata[7:0];
+        if(byteenable & wdata[7]) filter20[8:0] <= {1'b1, wdata[7:0]};
+        else if(byteenable & (~wdata[7])) filter20[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd16)) begin
-        if(byteenable) filter21[7:0] <= wdata[7:0];
+        if(byteenable & wdata[7]) filter21[8:0] <= {1'b1, wdata[7:0]};
+        else if(byteenable & (~wdata[7])) filter21[8:0] <= {1'b0, wdata[7:0]};
     end
 always @ (posedge clk)
     if(cs & write & (addr == 5'd17)) begin
-        if(byteenable) filter22[7:0] <= wdata[7:0];
+        if(byteenable & wdata[7]) filter22[8:0] <= {1'b1, wdata[7:0]};
+        else if(byteenable & (~wdata[7])) filter22[8:0] <= {1'b0, wdata[7:0]};
     end
 
 always @ (posedge clk)
@@ -137,7 +147,7 @@ always @ (posedge clk)
                 5'd16 : rdata <= filter21;
                 5'd17 : rdata <= filter22;
                 5'd18 : rdata <= conv_result[7:0];
-                default: rdata <= 8'dx;
+                default: rdata <= 32'dx;
             endcase
 
 
